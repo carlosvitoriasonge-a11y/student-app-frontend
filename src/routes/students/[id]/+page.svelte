b8ad54770ee92e597e576f57be4f3a6b0210dca8
Carlos Yochimi Ota <carlos@CarlosnoMacBook-Pro.local>
2026-01-30 13:18:02 +0900

    fix: corrigido HTML quebrado no +page.svelte

--- src/routes/students/[id]/+page.svelte
+++ src/routes/students/[id]/+page.svelte
@@ -229,12 +229,12 @@
         <tr><th>住所②</th><td>{student.address2}</td></tr>
         <tr><th>出身中学</th><td>{student.junior_high}</td></tr>
         <tr><th>中学校卒業年度</th><td> {student.junior_high_grad_date || "-"}（{toWareki(student.junior_high_grad_date)}）</td></tr>
-        <tr><th>編入学</th>{student.junior_high_grad_date || "-"}（{toWareki(student.junior_high_grad_date)}）</td></tr>
-          <td>{student.transfer_advanced_date || "-"}</td>
+        <tr><th>編入学</th>
+          <td>{student.transfer_advanced_date || "-"}（{toWareki(student.transfer_advanced_date)}）</td>
         </tr>
         
-        <tr><th>転入学</th>{student.junior_high_grad_date || "-"}（{toWareki(student.junior_high_grad_date)}）</td></tr>
-          <td>{student.transfer_date || "-"}</td>
+        <tr><th>転入学</th>
+          <td>{student.transfer_date || "-"}（{toWareki(student.transfer_date)}）</td>
         </tr>
         
         <tr><th>前在籍校</th>
